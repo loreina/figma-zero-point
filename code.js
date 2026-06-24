346e8df3dd7850ad1277a5e8200d22c57d24d26c
Loreina Chew <loreina.chew@gmail.com>
2026-06-24 16:51:36 -0700

    fix: use offset instead of grouping for repositioning

--- code.js
+++ code.js
@@ -1,15 +1,21 @@
-// Group all nodes on current page
-const group = figma.group(figma.currentPage.children, figma.currentPage);
-// Set axis to 0, 0
-group.x = 0;
-group.y = 0;
-// Ungroup all nodes
-group.children.forEach((i) => {
-    let parent = group.parent;
-    let index = parent.children.indexOf(group);
-    parent.insertChild(index, i);
-});
-// Adjust viewport to zoom to fit
-figma.viewport.scrollAndZoomIntoView([group]);
-// Close plugin
-figma.closePlugin();
+const children = [...figma.currentPage.children];
+
+if (children.length === 0) {
+  figma.closePlugin("No nodes on this page.");
+} else {
+  let minX = Infinity;
+  let minY = Infinity;
+
+  for (const node of children) {
+    minX = Math.min(minX, node.x);
+    minY = Math.min(minY, node.y);
+  }
+
+  for (const node of children) {
+    node.x -= minX;
+    node.y -= minY;
+  }
+
+  figma.viewport.scrollAndZoomIntoView(children);
+  figma.closePlugin();
+}
